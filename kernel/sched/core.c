0f56b55af253ad85eed70812ed7aaa4931cf5250
alucard24 <dmbaoh2@gmail.com>
2016-10-31 21:07:37 +0100

    sched/core: Revert part of Import LG H820 v10c into LA.HB.1.1.1-02740-8x96.0 commit.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3107,12 +3107,8 @@ void preempt_count_add(int val)
 	/*
 	 * Underflow?
 	 */
-	if (preempt_count() < 0) {
-		printk(KERN_ERR "%s: %d < 0\n",
-				__func__, preempt_count());
-		DEBUG_LOCKS_WARN_ON(1);
+	if (DEBUG_LOCKS_WARN_ON((preempt_count() < 0)))
 		return;
-	}
 #endif
 	__preempt_count_add(val);
 #ifdef CONFIG_DEBUG_PREEMPT
@@ -3139,12 +3135,8 @@ void preempt_count_sub(int val)
 	/*
 	 * Underflow?
 	 */
-	if (val > preempt_count()) {
-		printk(KERN_ERR "%s: %d > %d\n",
-				__func__, val, preempt_count());
-		DEBUG_LOCKS_WARN_ON(1);
+	if (DEBUG_LOCKS_WARN_ON(val > preempt_count()))
 		return;
-	}
 	/*
 	 * Is the spinlock portion underflowing?
 	 */
