0a3e1b0aa53c78c27eb7a1320971c1ee43df44aa
Arumuga Durai A <cadurai@codeaurora.org>
2016-11-25 17:21:38 +0530

    USB: dwc3: Fix usb host mode device rebooting in loop issue
    USB driver should take care of enabling required clocks for
    accessing AHB2PHY register. Currently driver is not enabling
    USB core clock before accessing AHB2PHY register during probe,
    which is required. This could cause device crash. Hence fix it
    by enabling required USB core clock as well before configuring
    AHB2PHY register in probe().
    
    CRs-Fixed: 1085406
    Change-Id: I9f971cbd54cabbf0119ed00bd811fcf2d7782e05
    Signed-off-by: Arumuga Durai A <cadurai@codeaurora.org>

--- drivers/usb/dwc3/dwc3-msm.c
+++ drivers/usb/dwc3/dwc3-msm.c
@@ -3946,6 +3946,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 			 */
 			dwc3_msm_config_gdsc(mdwc, 1);
 			clk_prepare_enable(mdwc->iface_clk);
+			clk_prepare_enable(mdwc->core_clk);
 			clk_prepare_enable(mdwc->cfg_ahb_clk);
 			/* Configure AHB2PHY for one wait state read/write*/
 			val = readl_relaxed(mdwc->ahb2phy_base +
@@ -3958,6 +3959,7 @@ static int dwc3_msm_probe(struct platform_device *pdev)
 				mb();
 			}
 			clk_disable_unprepare(mdwc->cfg_ahb_clk);
+			clk_disable_unprepare(mdwc->core_clk);
 			clk_disable_unprepare(mdwc->iface_clk);
 			dwc3_msm_config_gdsc(mdwc, 0);
 		}
