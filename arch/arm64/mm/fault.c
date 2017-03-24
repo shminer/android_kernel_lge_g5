CAF-staging/msm8916_kernel

arm64: Allow cache maintenance operations to trigger write faults
arm64 sets non-dirty userspace PTEs as read only by default.
The expectation is that on the first write the fault handler will
properly unmark the PTE as read only. Currently, the fault handler
will not mark a fault coming from a cache maintainece operation as
a write. The result of this is that a write to a user space address
from a cache mainenance operation will not get unmarked as read only
and the access may be retried infinitely. Rather than going through
the hassel and side effects of adding the cache operations to the
exception table and changing arm code to match, just drop the CM
check from the fault handler and let cache maintainence faults be
handled like normal writes.

Change-Id: I4b295b47ebdd24abf94204ad634e91ca5f71ce8a
Signed-off-by: Laura Abbott <lauraa@codeaurora.org>

arch/arm64/mm/fault.c
@@ -218,7 +218,7 @@ static int __kprobes do_page_fault(unsigned long addr, unsigned int esr,
 
 	if (esr & ESR_LNX_EXEC) {
 		vm_flags = VM_EXEC;
-	} else if ((esr & ESR_WRITE) && !(esr & ESR_CM)) {
+	} else if (esr & ESR_WRITE) {
 		vm_flags = VM_WRITE;
 		mm_flags |= FAULT_FLAG_WRITE;
 	}

shminer/android_kernel_lge_g5

arch/arm64/mm/fault.c
@@ -226,7 +226,7 @@ static int __kprobes do_page_fault(unsigned long addr, unsigned int esr,
 
 	if (esr & ESR_LNX_EXEC) {
 		vm_flags = VM_EXEC;
-	} else if ((esr & ESR_EL1_WRITE) && !(esr & ESR_EL1_CM)) {
+	} else if (esr & ESR_EL1_WRITE) {
 		vm_flags = VM_WRITE;
 		mm_flags |= FAULT_FLAG_WRITE;
 	}