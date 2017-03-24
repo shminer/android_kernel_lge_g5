6f8d59cd6bdd6c7aa78f5763bf97c9e57da468a4
shminer <a332574643@gmail.com>
2017-03-24 19:34:03 +0800

    dwc3: gadget: clean up

--- drivers/usb/dwc3/gadget.c
+++ drivers/usb/dwc3/gadget.c
@@ -2046,13 +2046,9 @@ static int dwc3_gadget_pullup(struct usb_gadget *g, int is_on)
 	ret = dwc3_gadget_run_stop(dwc, is_on, false);
 
 	spin_unlock_irqrestore(&dwc->lock, flags);
-#ifndef CONFIG_LGE_USB_G_ANDROID
+
 	pm_runtime_mark_last_busy(dwc->dev);
 	pm_runtime_put_autosuspend(dwc->dev);
-#else
-	pm_runtime_put(dwc->dev);
-#endif
-
 	dbg_event(0xFF, "Pullup put",
 		atomic_read(&dwc->dev->power.usage_count));
 
