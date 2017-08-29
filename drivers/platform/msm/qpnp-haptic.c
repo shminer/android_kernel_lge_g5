15582f682dfb439fe326559d8524c9f6bc3af37c
shminer <a332574643@gmail.com>
2017-08-29 10:37:15 +0800

    qpnp-haptic: reduce log

--- drivers/platform/msm/qpnp-haptic.c
+++ drivers/platform/msm/qpnp-haptic.c
@@ -521,10 +521,11 @@ static int qpnp_hap_play(struct qpnp_hap *hap, int on)
 			QPNP_HAP_PLAY_REG(hap->base));
 	if (rc < 0)
 		return rc;
+/*
 #ifdef CONFIG_LGE_QPNP_HAPTIC_OV_RB
 	dev_info(&hap->spmi->dev, "qpnp_hap_play: on = %d, voltage = %d \n", on, hap->vmax_mv);
 #endif
-
+*/
 	hap->reg_play = val;
 
 	return 0;
