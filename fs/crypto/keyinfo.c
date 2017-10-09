ecf9a5c3f776d77034362c77fd3f4d4fe6b09332
Eric Biggers <ebiggers@google.com>
2017-10-09 12:46:18 -0700

    UPSTREAM: fscrypt: fix dereference of NULL user_key_payload
    When an fscrypt-encrypted file is opened, we request the file's master
    key from the keyrings service as a logon key, then access its payload.
    However, a revoked key has a NULL payload, and we failed to check for
    this.  request_key() *does* skip revoked keys, but there is still a
    window where the key can be revoked before we acquire its semaphore.
    
    Fix it by checking for a NULL payload, treating it like a key which was
    already revoked at the time it was requested.
    
    Fixes: 88bd6ccdcdd6 ("ext4 crypto: add encryption key management facilities")
    Reviewed-by: James Morris <james.l.morris@oracle.com>
    Cc: <stable@vger.kernel.org>    [v4.1+]
    Signed-off-by: Eric Biggers <ebiggers@google.com>
    Signed-off-by: David Howells <dhowells@redhat.com>
    (cherry-picked from commit d60b5b7854c3d135b869f74fb93eaf63cbb1991a
     and fixed up for android-3.18)
    Change-Id: I7522324dc41969b5401fbe88ff90b4cd1f049cf2
    Signed-off-by: Eric Biggers <ebiggers@google.com>

--- fs/crypto/keyinfo.c
+++ fs/crypto/keyinfo.c
@@ -115,6 +115,11 @@ static int validate_user_key(struct fscrypt_info *crypt_info,
 	}
 	down_read(&keyring_key->sem);
 	ukp = ((struct user_key_payload *)keyring_key->payload.data);
+	if (!ukp) {
+		/* key was revoked before we acquired its semaphore */
+		res = -EKEYREVOKED;
+		goto out;
+	}
 	if (ukp->datalen != sizeof(struct fscrypt_key)) {
 		res = -EINVAL;
 		up_read(&keyring_key->sem);
